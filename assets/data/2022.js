7d464522bc3843fe68b35a21c8b4e197ef2e32bd
venkatesh <venkateshjuly1997@gmail.com>
2022-01-03 16:06:17 +0530

    intial push

---
+++ assets/data/2022.js
@@ -0,0 +1,59 @@
+let winners = {
+    name:'Team Winners',
+    image:'assets/img/hero-img.png'
+}
+let runners = {
+    name:'Team Runners',
+    image:'assets/img/hero-img.png'
+}
+let teamsList = [
+    {
+        team_id:1,
+        team_name: 'Team A'
+    },{
+        team_id:2,
+        team_name: 'Team B'
+    },{
+        team_id:3,
+        team_name: 'Team C'
+    },{
+        team_id:4,
+        team_name: 'Team D'
+    },{
+        team_id:5,
+        team_name: 'Team E'
+    }
+]
+let playersList = [
+    {
+        player_name: 'Venkatesh',
+        player_pic: 'assets/img/team/venkatesh.jpeg',
+        team_id: 1,
+        team_name:'Team A'
+    },{
+        player_name: 'Venkatesh',
+        player_pic: 'assets/img/team/venkatesh.jpeg',
+        team_id: 1,
+        team_name:'Team A'
+    },{
+        player_name: 'Venkatesh',
+        player_pic: 'assets/img/team/venkatesh.jpeg',
+        team_id: 2,
+        team_name:'Team B'
+    },{
+        player_name: 'Venkatesh',
+        player_pic: 'assets/img/team/venkatesh.jpeg',
+        team_id: 1,
+        team_name:'Team A'
+    },{
+        player_name: 'Venkatesh',
+        player_pic: 'assets/img/team/venkatesh.jpeg',
+        team_id: 1,
+        team_name:'Team A'
+    },{
+        player_name: 'Venkatesh',
+        player_pic: 'assets/img/team/venkatesh.jpeg',
+        team_id: 2,
+        team_name:'Team B'
+    },
+]
---
+++ assets/js/render-data.js
@@ -0,0 +1,51 @@
+var render_data = function () {
+    var onLoad = function () {
+        renderWinners();
+        renderRunners();
+        renderTeamsList();
+        renderTeamAndPlayers();
+    }
+    var renderWinners = function () {
+        $(".winner-name").text(winners.name);
+        $('.winner-image').attr('src', winners.image);
+    }
+    var renderRunners = function () {
+        $(".runner-name").text(runners.name);
+        $('.runner-image').attr('src', runners.image)
+
+    }
+    var renderTeamsList = function () {
+        let finalTeams = '';
+        teamsList.map((team, index) => {
+            let teamData = '<div class="col-lg-3 col-md-4 mt-4"><div class="icon-box"><i class="ri-store-line" style="color: #ffbb2c;"></i><h3><a href="">' + team.team_name + '</a></h3></div></div>'
+            finalTeams = finalTeams + teamData;
+        });
+        $('.team-data-container').html(finalTeams);
+    }
+    
+    var renderTeamHeaders = function () {
+        let initialHeader = '<li data-filter="*" class="filter-active">All</li>';
+        teamsList.map((team, index) => {
+            let teamHeader = '<li data-filter=".player-team-' + team.team_id + '">'+ team.team_name  +'</li>';
+            initialHeader = initialHeader + teamHeader;
+        });
+        $('#portfolio-flters').html(initialHeader)
+        
+    }
+    var renderTeamAndPlayers = function () {
+        renderTeamHeaders();
+        renderPlayers();
+    }
+    var renderPlayers = function () {
+        let allPlayers = '';
+        playersList.map((player, index) => {
+            let playerHtml = '<div class="col-lg-4 col-md-6 portfolio-item player-team-' + player.team_id + '"><div class="portfolio-wrap"><img src="' + player.player_pic + '" class="img-fluid" alt=""><div class="portfolio-info"><h4>' + player.player_name + '</h4><p>' + player.team_name + '</p></div></div></div>'
+            allPlayers = allPlayers + playerHtml
+        });
+        $('.portfolio-container').html(allPlayers)
+    }
+    return {
+        init: onLoad()
+    }
+}();
+render_data.init();
