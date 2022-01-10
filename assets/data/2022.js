bf18123c5a7784d08ad379d12444b941b5b697cb
manulalapeta-cricketclub <97011695+manulalapeta-cricketclub@users.noreply.github.com>
2022-01-10 11:05:04 +0530

    Update 2022.js
    12 team members dummy data

--- assets/data/2022.js
+++ assets/data/2022.js
@@ -24,18 +24,156 @@ let teamsList = [
         team_name: 'Team E'
     }
 ]
-let playersList = [
-    {
-        player_name: 'Kasa Venkatesh',
-        player_pic: 'assets/img/team/kasa.jpeg',
-        team_id: 1,
-        team_name:'Team A'
-    },
-    {
-        player_name: 'Teja',
-        player_pic: 'assets/img/team/Teja_prev_ui.png',
-        team_id: 1,
-        team_name:'Team A'
+let playersList=[
+	{
+		player_name: 'KasaVenkatesh',
+		player_pic: 'assets/img/team/kasa.jpeg',
+		team_id: 1,
+		team_name: 'TeamA'
+	},
+	{
+		player_name: 'KasaVenkatesh',
+		player_pic: 'assets/img/team/kasa.jpeg',
+		team_id: 1,
+		team_name: 'TeamA'
+	},
+	{
+		player_name: 'KasaVenkatesh',
+		player_pic: 'assets/img/team/kasa.jpeg',
+		team_id: 1,
+		team_name: 'TeamA'
+	},
+	{
+		player_name: 'KasaVenkatesh',
+		player_pic: 'assets/img/team/kasa.jpeg',
+		team_id: 1,
+		team_name: 'TeamA'
+	},
+	{
+		player_name: 'KasaVenkatesh',
+		player_pic: 'assets/img/team/kasa.jpeg',
+		team_id: 1,
+		team_name: 'TeamA'
+	},
+	{
+		player_name: 'KasaVenkatesh',
+		player_pic: 'assets/img/team/kasa.jpeg',
+		team_id: 1,
+		team_name: 'TeamA'
+	},
+	{
+		player_name: 'KasaVenkatesh',
+		player_pic: 'assets/img/team/kasa.jpeg',
+		team_id: 1,
+		team_name: 'TeamA'
+	},
+	{
+		player_name: 'KasaVenkatesh',
+		player_pic: 'assets/img/team/kasa.jpeg',
+		team_id: 1,
+		team_name: 'TeamA'
+	},
+	{
+		player_name: 'KasaVenkatesh',
+		player_pic: 'assets/img/team/kasa.jpeg',
+		team_id: 1,
+		team_name: 'TeamA'
+	},
+	{
+		player_name: 'KasaVenkatesh',
+		player_pic: 'assets/img/team/kasa.jpeg',
+		team_id: 1,
+		team_name: 'TeamA'
+	},
+	{
+		player_name: 'KasaVenkatesh',
+		player_pic: 'assets/img/team/kasa.jpeg',
+		team_id: 1,
+		team_name: 'TeamA'
+	},
+	{
+		player_name: 'KasaVenkatesh',
+		player_pic: 'assets/img/team/kasa.jpeg',
+		team_id: 1,
+		team_name: 'TeamA'
+	},
+	{
+          player_name: 'Yeshwanth',
+          player_pic: 'assets/img/team/yeshwanth.png',
+          team_id: 2,
+          team_name:'Team B'
+      },
+      {
+          player_name: 'Yeshwanth',
+          player_pic: 'assets/img/team/yeshwanth.png',
+          team_id: 2,
+          team_name:'Team B'
+      },
+      {
+          player_name: 'Yeshwanth',
+          player_pic: 'assets/img/team/yeshwanth.png',
+          team_id: 2,
+          team_name:'Team B'
+      },
+      {
+          player_name: 'Yeshwanth',
+          player_pic: 'assets/img/team/yeshwanth.png',
+          team_id: 2,
+          team_name:'Team B'
+      },
+      {
+          player_name: 'Yeshwanth',
+          player_pic: 'assets/img/team/yeshwanth.png',
+          team_id: 2,
+          team_name:'Team B'
+      },
+      {
+          player_name: 'Yeshwanth',
+          player_pic: 'assets/img/team/yeshwanth.png',
+          team_id: 2,
+          team_name:'Team B'
+      },
+      {
+          player_name: 'Yeshwanth',
+          player_pic: 'assets/img/team/yeshwanth.png',
+          team_id: 2,
+          team_name:'Team B'
+      },
+      {
+          player_name: 'Yeshwanth',
+          player_pic: 'assets/img/team/yeshwanth.png',
+          team_id: 2,
+          team_name:'Team B'
+      },
+      {
+          player_name: 'Yeshwanth',
+          player_pic: 'assets/img/team/yeshwanth.png',
+          team_id: 2,
+          team_name:'Team B'
+      },
+      {
+          player_name: 'Yeshwanth',
+          player_pic: 'assets/img/team/yeshwanth.png',
+          team_id: 2,
+          team_name:'Team B'
+      },
+      {
+          player_name: 'Yeshwanth',
+          player_pic: 'assets/img/team/yeshwanth.png',
+          team_id: 2,
+          team_name:'Team B'
+      },
+      {
+          player_name: 'Yeshwanth',
+          player_pic: 'assets/img/team/yeshwanth.png',
+          team_id: 2,
+          team_name:'Team B'
+      },
+      {
+        player_name: 'Sarat',
+        player_pic: 'assets/img/team/sarat.png',
+        team_id: 3,
+        team_name:'Team C'
     },
     {
         player_name: 'Sarat',
@@ -46,25 +184,207 @@ let playersList = [
     {
         player_name: 'Sarat',
         player_pic: 'assets/img/team/sarat.png',
-        team_id: 2,
-        team_name:'Team B'
+        team_id: 3,
+        team_name:'Team C'
+    },
+    {
+        player_name: 'Sarat',
+        player_pic: 'assets/img/team/sarat.png',
+        team_id: 3,
+        team_name:'Team C'
+    },
+    {
+        player_name: 'Sarat',
+        player_pic: 'assets/img/team/sarat.png',
+        team_id: 3,
+        team_name:'Team C'
+    },
+    {
+        player_name: 'Sarat',
+        player_pic: 'assets/img/team/sarat.png',
+        team_id: 3,
+        team_name:'Team C'
+    },
+    {
+        player_name: 'Sarat',
+        player_pic: 'assets/img/team/sarat.png',
+        team_id: 3,
+        team_name:'Team C'
+    },
+    {
+        player_name: 'Sarat',
+        player_pic: 'assets/img/team/sarat.png',
+        team_id: 3,
+        team_name:'Team C'
+    },
+    {
+        player_name: 'Sarat',
+        player_pic: 'assets/img/team/sarat.png',
+        team_id: 3,
+        team_name:'Team C'
+    },
+    {
+        player_name: 'Sarat',
+        player_pic: 'assets/img/team/sarat.png',
+        team_id: 3,
+        team_name:'Team C'
+    },
+    {
+        player_name: 'Sarat',
+        player_pic: 'assets/img/team/sarat.png',
+        team_id: 3,
+        team_name:'Team C'
+    },
+    {
+        player_name: 'Sarat',
+        player_pic: 'assets/img/team/sarat.png',
+        team_id: 3,
+        team_name:'Team C'
+    },
+    {
+        player_name: 'Teja',
+        player_pic: 'assets/img/team/Teja_prev_ui.png',
+        team_id: 4,
+        team_name:'Team D'
     },
      {
-         player_name: 'Chandhu',
-         player_pic: 'assets/img/team/chandhu.png',
-         team_id: 2,
-         team_name:'Team B'
+        player_name: 'Teja',
+        player_pic: 'assets/img/team/Teja_prev_ui.png',
+        team_id: 4,
+        team_name:'Team D'
+    },
+     {
+        player_name: 'Teja',
+        player_pic: 'assets/img/team/Teja_prev_ui.png',
+        team_id: 4,
+        team_name:'Team D'
+    },
+     {
+        player_name: 'Teja',
+        player_pic: 'assets/img/team/Teja_prev_ui.png',
+        team_id: 4,
+        team_name:'Team D'
+    },
+     {
+        player_name: 'Teja',
+        player_pic: 'assets/img/team/Teja_prev_ui.png',
+        team_id: 4,
+        team_name:'Team D'
+    },
+     {
+        player_name: 'Teja',
+        player_pic: 'assets/img/team/Teja_prev_ui.png',
+        team_id: 4,
+        team_name:'Team D'
+    },
+     {
+        player_name: 'Teja',
+        player_pic: 'assets/img/team/Teja_prev_ui.png',
+        team_id: 4,
+        team_name:'Team D'
+    },
+     {
+        player_name: 'Teja',
+        player_pic: 'assets/img/team/Teja_prev_ui.png',
+        team_id: 4,
+        team_name:'Team D'
+    },
+     {
+        player_name: 'Teja',
+        player_pic: 'assets/img/team/Teja_prev_ui.png',
+        team_id: 4,
+        team_name:'Team D'
+    },
+     {
+        player_name: 'Teja',
+        player_pic: 'assets/img/team/Teja_prev_ui.png',
+        team_id: 4,
+        team_name:'Team D'
+    },
+     {
+        player_name: 'Teja',
+        player_pic: 'assets/img/team/Teja_prev_ui.png',
+        team_id: 4,
+        team_name:'Team D'
+    },
+     {
+        player_name: 'Teja',
+        player_pic: 'assets/img/team/Teja_prev_ui.png',
+        team_id: 4,
+        team_name:'Team D'
+    },
+    {
+           player_name: 'Sai Kiran',
+           player_pic: 'assets/img/team/saikiran.png',
+           team_id: 5,
+           team_name:'Team E'
      },
-      {
-          player_name: 'Yeshwanth',
-          player_pic: 'assets/img/team/yeshwanth.png',
-          team_id: 2,
-          team_name:'Team B'
-      },
+    {
+           player_name: 'Sai Kiran',
+           player_pic: 'assets/img/team/saikiran.png',
+           team_id: 5,
+           team_name:'Team E'
+       },
        {
            player_name: 'Sai Kiran',
            player_pic: 'assets/img/team/saikiran.png',
-           team_id: 2,
-           team_name:'Team B'
-       }
+           team_id: 5,
+           team_name:'Team E'
+       },
+       {
+           player_name: 'Sai Kiran',
+           player_pic: 'assets/img/team/saikiran.png',
+           team_id: 5,
+           team_name:'Team E'
+       },
+       {
+           player_name: 'Sai Kiran',
+           player_pic: 'assets/img/team/saikiran.png',
+           team_id: 5,
+           team_name:'Team E'
+       },
+       {
+           player_name: 'Sai Kiran',
+           player_pic: 'assets/img/team/saikiran.png',
+           team_id: 5,
+           team_name:'Team E'
+       },
+       {
+           player_name: 'Sai Kiran',
+           player_pic: 'assets/img/team/saikiran.png',
+           team_id: 5,
+           team_name:'Team E'
+       },
+       {
+           player_name: 'Sai Kiran',
+           player_pic: 'assets/img/team/saikiran.png',
+           team_id: 5,
+           team_name:'Team E'
+       },
+       {
+           player_name: 'Sai Kiran',
+           player_pic: 'assets/img/team/saikiran.png',
+           team_id: 5,
+           team_name:'Team E'
+       },
+       {
+           player_name: 'Sai Kiran',
+           player_pic: 'assets/img/team/saikiran.png',
+           team_id: 5,
+           team_name:'Team E'
+       },
+       {
+           player_name: 'Sai Kiran',
+           player_pic: 'assets/img/team/saikiran.png',
+           team_id: 5,
+           team_name:'Team E'
+       },
+       {
+           player_name: 'Sai Kiran',
+           player_pic: 'assets/img/team/saikiran.png',
+           team_id: 5,
+           team_name:'Team E'
+       },
+       	
+	
 ]
